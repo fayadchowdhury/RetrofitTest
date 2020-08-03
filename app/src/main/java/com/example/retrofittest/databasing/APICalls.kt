c3eb895fb4eef8b2852c5c92f398b5f30af4da5b
nafiz <naafizrahman1@gmail.com>
2020-08-03 18:05:08 +0600

    APICalls interface updated.

--- app/src/main/java/com/example/retrofittest/databasing/APICalls.kt
+++ app/src/main/java/com/example/retrofittest/databasing/APICalls.kt
@@ -34,12 +34,84 @@ interface APICalls {
 
     //patient routes
 
+    @POST("patient/findById")
+    fun getPatientById(@Body params: RequestBody): Call<ResponseBody>
+
+    @POST("patient/updateProfile")
+    fun updatePatientProfileById(@Header("Authorization") jwt: String, @Body params: RequestBody): Call<ResponseBody>
+
+    @POST("patient/deleteById")
+    fun deletePatientById(@Header("Authorization") jwt: String, @Body params: RequestBody): Call<ResponseBody>
 
     //slot routes
+
+    @POST("slot/create")
+    fun createSlot(@Header("Authorization") jwt: String, @Body params: RequestBody): Call<ResponseBody>
+
+    @POST("slot/getSlotById")
+    fun getSlotById(@Body params: RequestBody): Call<ResponseBody>
+
+    @POST("slot/viewAllSlotsByDoctor")
+    fun viewAllSlotsByDoctor(@Body params: RequestBody): Call<ResponseBody>
+
+    @POST("slot/deleteSlotByDoctorId")
+    fun deleteSlotByDoctorId(@Header("Authorization") jwt: String, @Body params: RequestBody): Call<ResponseBody>
+
+    @POST("slot/deleteSlotId")
+    fun deleteSlotId(@Header("Authorization") jwt: String, @Body params: RequestBody): Call<ResponseBody>
 
 
     //appointment routes
 
+    @POST("appointment/createAppointment")
+    fun createAppointment(@Header("Authorization") jwt: String, @Body params: RequestBody): Call<ResponseBody>
+
+    @POST("appointment/viewAppointment")
+    fun  viewAppointmentById(@Body params: RequestBody): Call<ResponseBody>
+
+
+    @POST("appointment/viewPastAppointmentsPatient")
+    fun viewPastAppointmentsPatient(@Header("Authorization") jwt: String, @Body params: RequestBody): Call<ResponseBody>
+
+    @POST("appointment/viewPastAppointmentsDoctor")
+    fun viewPastAppointmentsDoctor(@Header("Authorization") jwt: String, @Body params: RequestBody): Call<ResponseBody>
+
+    @POST("appointment/viewUpcomingAppointmentsPatient")
+    fun viewUpcomingAppointmentsPatient(@Body params: RequestBody): Call<ResponseBody>
+
+    @POST("appointment/viewUpcomingAppointmentsDoctor")
+    fun viewUpcomingAppointmentsDoctor(@Body params: RequestBody): Call<ResponseBody>
+
+    @POST("appointment/deleteAppointment")
+    fun deleteAppointmentById(@Header("Authorization") jwt: String, @Body params: RequestBody): Call<ResponseBody>
+
+    @POST("appointment/completeAppointment")
+    fun completeAppointment(@Header("Authorization") jwt: String, @Body params: RequestBody): Call<ResponseBody>
+
+    @POST("appointment/updatePrescription")
+    fun editPrescription(@Header("Authorization") jwt: String, @Body params: RequestBody): Call<ResponseBody>
 
     //rating routes
+
+    @POST("rating/getById")
+    fun getRatingsById(@Body params: RequestBody): Call<ResponseBody>
+
+    @POST("rating/updateById")
+    fun editRatingsById(@Header("Authorization") jwt: String, @Body params: RequestBody): Call<ResponseBody>
+
+    //auth routes
+
+    @POST("auth/signup/doctor")
+    fun registerDoctorBasic(@Body params: RequestBody): Call<ResponseBody>
+
+    @POST("auth/signup/patient")
+    fun registerPatientBasic(@Body params: RequestBody): Call<ResponseBody>
+
+    @POST("auth/login/doctor")
+    fun loginDoctor(@Body params: RequestBody): Call<ResponseBody>
+
+    @POST("auth/login/patient")
+    fun loginPatient(@Body params: RequestBody): Call<ResponseBody>
+
+
 }
